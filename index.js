b43a23d631c4b3686d54eca907aa363366337967
zhangyunnian <zhangyunnian@bytedance.com>
2020-04-13 10:36:25 +0800

    modify

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const reactConverter = require('./src/convers/reactConverter');
 const vueConverter = require('./src/convers/vueConverter');
 
 module.exports = function(source) {
-  const options = loaderUtils.getOptions(this);
+  const options = loaderUtils.getOptions(this) || {};
+  const reg = options.test || /.*/;  
+  if (!reg.test(this.resourcePath)) return source;
+
+  console.log(`will gen css for file: ${this.resourcePath}`)
   const type = (options.type || 'vue').toLowerCase();
   const converter = type === 'vue' ? vueConverter : reactConverter;
   const result = converter.handle(source, options);
